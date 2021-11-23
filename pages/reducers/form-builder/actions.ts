ba9089a65d61f18837e6c751b5941de7f85db8e7
Herriold <ratsimbazafyherrio@gmail.com>
2021-11-23 16:51:46 +0300

    clean code

--- pages/reducers/form-builder/actions.ts
+++ pages/reducers/form-builder/actions.ts
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getDataFormBuilder = createAsyncThunk('kanye/kanyeQuote', async () => {
-  const response = await axios.get('https://api.kanye.rest/');
+export const getDataFormBuilder = createAsyncThunk('form/formBuilder', async () => {
+  const response = await axios.get('https://localhost:4001/');
 
   return response.data;
 });
